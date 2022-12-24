ad10aed3a18235d32242938e802ed134233fa6b4
YounesAO <younes.aitouahda@gmail.com>
2022-12-24 20:43:00 +0000

    club ADMIN ADD

--- accept.php
+++ accept.php
@@ -5,11 +5,14 @@ require 'dbConnect.php';
 $value=$_GET['value'];
 $id = $_GET['id'];
 if($value=true){
-    $query = "UPDATE integrer SET valide=1 WHERE (idEtudiant=$id) and (idClub =$Club); ";
+    $query = "UPDATE integrer SET valide=1 WHERE (idEtudiant=$id) and (idClub =$Club);"; 
+    $query1 = "UPDATE etudiant SET role=$Club WHERE (idEtudiant=$id) ;
+    ";
 
 // Execute the query
 if (mysqli_query($con, $query)) {
     echo "Record updated successfully";
+    mysqli_query($con, $query1);
 } else {
     echo "Error updating record: " . mysqli_error($con);
 }
