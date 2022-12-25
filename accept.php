bbd2ca1c8c6c0201ada24939f52cf9e304aa15da
YounesAO <younes.aitouahda@gmail.com>
2022-12-25 22:12:28 +0000

    slide images /accept role

--- accept.php
+++ accept.php
@@ -6,13 +6,10 @@ $value=$_GET['value'];
 $id = $_GET['id'];
 if($value=true){
     $query = "UPDATE integrer SET valide=1 WHERE (idEtudiant=$id) and (idClub =$Club);"; 
-    $query1 = "UPDATE etudiant SET role=$Club WHERE (idEtudiant=$id) ;
-    ";
 
 // Execute the query
 if (mysqli_query($con, $query)) {
     echo "Record updated successfully";
-    mysqli_query($con, $query1);
 } else {
     echo "Error updating record: " . mysqli_error($con);
 }
